8da83976075969b59a06f06fa0bc733fd2a1b6cc
karthikbala07 <41923434+karthikbala07@users.noreply.github.com>
2020-06-10 17:43:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def results():
     if (req.get('queryResult').get('action') == 'ug'):
          return {'fulfillmentText': 'I did my under graduate studies in SSN college of Engineering'}
     elif (req.get('queryResult').get('action') == 'ug_grade'):
-         return {'fulfillmentText': 'My cumulative score is 7.2 out of 10 from university exams.'}
+         return {'fulfillmentText': 'My Bachelors cumulative score is 7.2 out of 10 from university exams.'}
     elif (req.get('queryResult').get('action') == 'ug_name'):
          return {'fulfillmentText': 'My course was  B.E in electrical engineering .'}
     elif (req.get('queryResult').get('action') == 'school'):
@@ -28,7 +28,7 @@ def results():
     elif (req.get('queryResult').get('action') == 'pg'):
          return {'fulfillmentText': 'I did my graduate studies in State University of New York,Buffalo '}
     elif (req.get('queryResult').get('action') == 'pg_grade'):
-         return {'fulfillmentText': 'My course was  B.E in electrical engineering .'}
+         return {'fulfillmentText': 'My Masters cumulative score is 3.8 out of 4 from the final exams'}
     elif (req.get('queryResult').get('action') == 'pg_name'):
          return {'fulfillmentText': 'My course was  M.S in electrical engineering with a focus in Semiconductors .'}
          
